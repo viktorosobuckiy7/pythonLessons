3046e875ffc27ce9b1ecb011307557aa55519990
viktorosobuckiy7 <106606795+viktorosobuckiy7@users.noreply.github.com>
2022-08-02 14:23:18 +0300

    Add files via upload

---
+++ regular.py
@@ -0,0 +1,26 @@
+import re
+
+# s = input("Введите строку")
+# result = re.search(r'Rb+r', s)
+# print(result)
+
+# pattern = re.compile(r"^[a-z0-9]{2,10}$", re.I)
+#
+#
+# def validate(name: str):
+#     return bool(pattern.match(name))
+#
+#
+# print(validate("d")
+
+
+# p = r'\b[A-Za-z0-9_%+-?]+@[A-Za-z0-9]+\.[A-Z|a-z]{2,}\b'
+#
+#
+# def check(email):
+#     if (re.fullmatch(p, email)):
+#         print("Valid Email")
+#
+#     else:
+#         print("Invalid Email")
+
